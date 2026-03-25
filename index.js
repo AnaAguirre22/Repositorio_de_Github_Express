386135d85b4499bc1e0fea8ba5c4026afaa651c7
AnaAguirre22 <aguirresanchezanarubi22@gmail.com>
2026-03-24 19:33:00 -0700

    Agregado endpoint /usuario con respuesta JSON

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+  res.send('API funcionando correctamente');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Ana Aguirre', 
+    rol: 'Estudiante de Informática UAS'
+  };
+
+  res.json(usuario);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
